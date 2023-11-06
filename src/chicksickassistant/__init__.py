61ccc2964163d479153b1d473ce1c76323d865b4
VINAYAK <vinayak@192.168.1.12>
2023-11-06 11:02:23 +0530

    requirements added

--- src/chicksickassistant/__init__.py
+++ src/chicksickassistant/__init__.py
@@ -17,4 +17,4 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ]
 )
-logger = logging.getLogger(chicksickassistantLogger)
+logger = logging.getLogger("chicksickassistantLogger")
